6af6b9b2494e3f9aaddeecef769f666c4a79fe81
Jndrs <junior.andres2111@gmail.com>
2025-08-09 15:45:44 -0500

    tercer commit desde local

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import cv2
-img = cv2.imread("imagen.jpg")
+img = cv2.imread("images.jpeg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv2.imwrite("gray_image.jpg", gray)
+cv2.imwrite("gray_image.jpeg", gray)    
